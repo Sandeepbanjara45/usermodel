c6eb228f7e6549d62a7d6cd788b70f57a00ed0a7
Sandeepbanjara45 <sandeepbanjara1999@gmail.com>
2023-11-09 21:48:06 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Search from './Search'
+import Pagination from './Pagination'
+import Stories from './Stories'
+import "./App.css";
+// import { AppContext} from './Context'
+// import React, { useContext } from 'react'
+
+const App = () => {
+
+
+  return (
+    <>
+      <Search/>
+      <Pagination/>
+      <Stories/>
+    </>
+  )
+}
+
+export default App
